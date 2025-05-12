1c8825c4f61cfc3a52b94c58642b05cedd680b35
TheFriedHashbrown <amygreat0@gmail.com>
2025-05-12 22:34:16 +0530

    Emojis project

---
+++ JS/Emojis/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("#emoji")
+
+const emojis = [
+    "😊","🤣","😒","😍","😁","😎","😉","😢","😜"
+]
+
+btn.addEventListener("mouseover", () => {
+    btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)]
+})
